fa1c5305f17693e246846da1fe343a0e5cc742c8
rearman <rearman@r717.net>
2023-01-09 09:31:40 -0500

    Move back to package.el from straight
    Moving away from straight due to issues with magit after update.  Moving some
    things to early-init.el (same things Prot. has in his)

--- early-init.el
+++ early-init.el
@@ -1,1 +1,13 @@
-(setq package-enable-at-startup nil)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq default-directory "~/"
+      inhibit-splash-screen t
+      inhibit-startup-screen t
+      inhibit-startup-message t
+      inhibit-startup-buffer-menu t
+      initial-scratch-message nil
+      server-client-instructions nil)
+
+(setq package-enable-at-startup t)
