a52f62826f087a6ab403e09df24ae4fb4e569415
rush <rush@Thonkbook.com>
2023-01-15 20:18:52 -0500

    Add reports for ledger-mode

--- init.el
+++ init.el
@@ -25,6 +25,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ledger-reports
+   '(("assets" "%(binary) -f %(ledger-file) bal assets")
+     ("income" "%(binary) -f %(ledger-file) bal income")
+     ("expenses" "%(binary) -f %(ledger-file) bal expenses")
+     ("liabilities" "%(binary) -f %(ledger-file) bal liabilities")
+     ("bal" "%(binary) -f %(ledger-file) bal")
+     ("reg" "%(binary) -f %(ledger-file) reg")
+     ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
+     ("account" "%(binary) -f %(ledger-file) reg %(account)")))
  '(minibuffer-prompt-properties '(read-only t cursor-intangible t face minibuffer-prompt))
  '(package-selected-packages
    '(ledger-mode s openwith gnuplot-mode magit expand-region company use-package)))
