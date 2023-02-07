dffd6a1f6f22879ab46f05a025f6e8e8dcee366a
rearman <rearman@r717.net>
2023-02-07 08:33:03 -0500

    Add some Bindings from lem/acme, remove unused
    Removing rotate-windows and toggle-window-split, rarely used.
    
    Grabbed some bindings from lem
    <github.com/lem-project/lem/blob/master/src/default-keymap.lisp>.  C-h as
    backspace, C-x ? as describe-key, C-x / as describe-symbol.
    
    Some bindings inspired by acme:  Home goes to top of buffer, End goes to bottom
    of buffer, up/down act as page up/down.

--- defuns.el
+++ defuns.el
@@ -33,61 +33,6 @@ Can't go prev line first, edge case of beginning of buffer."
   (interactive)
   (join-line 0))
 
-(defun toggle-window-split ()
-  "If two windows are open, toggle their split layout between vert. and horiz.
-Stolen from http://whattheemacsd.com"
-  (interactive)
-  (when (= (count-windows) 2)
-      (let* ((this-win-buffer (window-buffer))
-	     (next-win-buffer (window-buffer (next-window)))
-	     (this-win-edges (window-edges (selected-window)))
-	     (next-win-edges (window-edges (next-window)))
-	     (this-win-2nd (not (and (<= (car this-win-edges)
-					 (car next-win-edges))
-				     (<= (cadr this-win-edges)
-					 (cadr next-win-edges)))))
-	     (splitter
-	      (if (= (car this-win-edges)
-		     (car (window-edges (next-window))))
-		  'split-window-horizontally
-		'split-window-vertically)))
-	(delete-other-windows)
-	(let ((first-win (selected-window)))
-	  (funcall splitter)
-	  (when this-win-2nd (other-window 1))
-	  (set-window-buffer (selected-window) this-win-buffer)
-	  (set-window-buffer (next-window) next-win-buffer)
-	  (select-window first-win)
-	  (when this-win-2nd (other-window 1))))))
-
-(defun rotate-windows ()
-  "Rotate your windows.
-Stolen from http://whattheemacsd.com"
-  (interactive)
-  (cond ((not (> (count-windows)1))
-	 (message "You can't rotate a single window!"))
-	((> (count-windows) 2)
-	 (message "You can't rotate more than two windows!"))
-	(t
-	 (setq i 1)
-	 (setq numWindows (count-windows))
-	 (while  (< i numWindows)
-	   (let* (
-		  (w1 (elt (window-list) i))
-		  (w2 (elt (window-list) (+ (% i numWindows) 1)))
-
-		  (b1 (window-buffer w1))
-		  (b2 (window-buffer w2))
-
-		  (s1 (window-start w1))
-		  (s2 (window-start w2))
-		  )
-	     (set-window-buffer w1  b2)
-	     (set-window-buffer w2 b1)
-	     (set-window-start w1 s2)
-	     (set-window-start w2 s1)
-	     (setq i (1+ i)))))))
-
 (defun smart-beginning-of-line ()
   "Move point to first non-whitespace character or beginning-of-line.
 If point was already at that position, move point to beginning of line.
