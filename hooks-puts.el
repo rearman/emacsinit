11b81a9103f4a9bdd71419631836864573735841
rearman <rearman@r717.net>
2023-03-01 08:58:18 -0500

    Add hooks to set tab-width,etc for c/lisp/elisp modes

--- hooks-puts.el
+++ hooks-puts.el
@@ -2,6 +2,15 @@
 ;; HOOKS
 (add-hook 'before-save-hook 'whitespace-cleanup)
 (add-hook 'dired-mode-hook 'dired-hide-details-mode t)
+(add-hook 'lisp-mode-hook (lambda () (setq tab-width 2)))
+(add-hook 'emacs-lisp-mode-hook (lambda () (setq tab-width 2)))
+(add-hook 'c-mode-common-hook (lambda ()
+								(c-set-style "k&r")
+								(setq tab-width 5
+									  c-basic-offset 5
+									  indent-tabs-mode t
+									  comment-column 48)))
+
 
 ;; PUTS AND PUSHES
 (put 'upcase-region 'disabled nil)
