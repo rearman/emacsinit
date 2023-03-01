19fcdeb9e9757b0fc6411e7adc76b73362c99032
rearman <rearman@r717.net>
2023-03-01 09:00:19 -0500

    Re-tab files, add defuns for doas/sudo with tramp

--- setq-defaults.el
+++ setq-defaults.el
@@ -1,40 +1,42 @@
 ;; -*- lexical-binding: t; -*-
 ;; SETQ AND DEFAULTS
 (setq-default indicate-empty-lines t
-	      fill-column 80
-	      read-file-name-completion-ignore-case t
-	      read-buffer-completion-ignore-case t
-	      cursor-type 'bar
-	      cursor-in-non-selected-windows nil)
+			  fill-column 80
+			  read-file-name-completion-ignore-case t
+			  read-buffer-completion-ignore-case t
+			  cursor-type 'bar
+			  cursor-in-non-selected-windows 'hollow)
 
 (setq require-final-newline t
-      text-quoting-style 'straight
-      ring-bell-function 'ignore
-      use-short-answers t
-      mode-line-compact t
-      make-backup-files nil
-      auto-save-default nil
-      confirm-nonexistent-file-or-buffer nil
-      show-paren-delay 0
-      show-paren-style 'expression
-      blink-matching-paren 'jump
-      global-hl-line-sticky-flag t
-      echo-keystrokes 0.01
-      save-interprogram-paste-before-kill t
-      confirm-kill-emacs 'y-or-n-p
-      confirm-kill-processes nil
-      eshell-destroy-buffer-when-process-dies t
-      custom-file (concat user-emacs-directory "custom-set-variables.el")
-      ediff-window-setup-function 'ediff-setup-windows-plain
-      ediff-split-window-function (if (> (frame-width) 150)
-					 'split-window-horizontally
-				    'split-window-vertically)
-      prettify-symbols-alist '(("lambda" . 955)
-			       ("delta" . 120517)
-			       ("epsilon" . 120518)
-			       ("->" . 8594)
-			       ("<=" . 8804)
-			       (">=" . 8805)))
+	  text-quoting-style 'straight
+	  ring-bell-function 'ignore
+	  use-short-answers t
+	  mode-line-compact t
+	  make-backup-files nil
+	  auto-save-default nil
+	  confirm-nonexistent-file-or-buffer nil
+	  show-paren-delay 0
+	  show-paren-style 'expression
+	  blink-matching-paren 'jump
+	  global-hl-line-sticky-flag t
+	  echo-keystrokes 0.01
+	  save-interprogram-paste-before-kill t
+	  confirm-kill-emacs 'y-or-n-p
+	  confirm-kill-processes nil
+	  frame-title-format "%b - EMACS"
+	  eshell-destroy-buffer-when-process-dies t
+	  dired-listing-switches "-alv --group-directories-first"
+	  custom-file (concat user-emacs-directory "custom-set-variables.el")
+	  ediff-window-setup-function 'ediff-setup-windows-plain
+	  ediff-split-window-function (if (> (frame-width) 150)
+									  'split-window-horizontally
+									'split-window-vertically)
+	  prettify-symbols-alist '(("lambda" . 955)
+							   ("delta" . 120517)
+							   ("epsilon" . 120518)
+							   ("->" . 8594)
+							   ("<=" . 8804)
+							   (">=" . 8805)))
 
 ;; OPTIONS
 (save-place-mode 1)
@@ -44,6 +46,7 @@
 (auto-fill-mode t)
 (midnight-mode t)
 (column-number-mode t)
+(file-name-shadow-mode t)
 (delete-selection-mode t)
 (global-hl-line-mode t)
 (global-prettify-symbols-mode t)
