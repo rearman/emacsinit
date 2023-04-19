6971dcce7e59178636d63e169de3004e96246507
rearman <rearman@r717.net>
2023-04-19 11:58:23 -0400

    Need to pull in custom.el (empty) from repo

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("021321ae56a45794f43b41de09fb2bfca184e196666b7d7ff59ea97ec2114559" default))
+ '(package-selected-packages
+   '(eink-theme eink-emacs visual-fill-column use-package slime orderless openwith magit ledger-mode gnuplot-mode expand-region corfu-terminal company browse-kill-ring)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
